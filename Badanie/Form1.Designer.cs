7a5796d1d00fda4344a1e58350bbe7b341a073c3
kaczks <mlodymaks@gmail.com>
2021-10-20 12:11:09 +0200

    Dodanie labelow do kolejki

--- Badanie/Form1.Designer.cs
+++ Badanie/Form1.Designer.cs
@@ -43,6 +43,19 @@ namespace Badanie
             this.Imie_label = new System.Windows.Forms.Label();
             this.Badanie_label = new System.Windows.Forms.Label();
             this.tp_Badania = new System.Windows.Forms.DateTimePicker();
+            this.Dane_pacjeta = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lKolejka = new System.Windows.Forms.Label();
+            this.btn_nastepny = new System.Windows.Forms.Button();
+            this.btn_poprzedni = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lImie
@@ -158,11 +171,141 @@ namespace Badanie
             this.tp_Badania.Size = new System.Drawing.Size(67, 23);
             this.tp_Badania.TabIndex = 11;
             // 
+            // Dane_pacjeta
+            // 
+            this.Dane_pacjeta.AutoSize = true;
+            this.Dane_pacjeta.Location = new System.Drawing.Point(573, 91);
+            this.Dane_pacjeta.Name = "Dane_pacjeta";
+            this.Dane_pacjeta.Size = new System.Drawing.Size(186, 15);
+            this.Dane_pacjeta.TabIndex = 12;
+            this.Dane_pacjeta.Text = "Dane pacjeta dodanego do kolejki";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(196, 396);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 358);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(196, 429);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(303, 358);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(303, 396);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 15);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(303, 429);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 15);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(413, 358);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 15);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(413, 396);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 15);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(413, 429);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(38, 15);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "label9";
+            // 
+            // lKolejka
+            // 
+            this.lKolejka.AutoSize = true;
+            this.lKolejka.Location = new System.Drawing.Point(303, 309);
+            this.lKolejka.Name = "lKolejka";
+            this.lKolejka.Size = new System.Drawing.Size(45, 15);
+            this.lKolejka.TabIndex = 22;
+            this.lKolejka.Text = "Kolejka";
+            // 
+            // btn_nastepny
+            // 
+            this.btn_nastepny.Location = new System.Drawing.Point(196, 478);
+            this.btn_nastepny.Name = "btn_nastepny";
+            this.btn_nastepny.Size = new System.Drawing.Size(75, 23);
+            this.btn_nastepny.TabIndex = 23;
+            this.btn_nastepny.Text = "Next";
+            this.btn_nastepny.UseVisualStyleBackColor = true;
+            // 
+            // btn_poprzedni
+            // 
+            this.btn_poprzedni.Location = new System.Drawing.Point(376, 478);
+            this.btn_poprzedni.Name = "btn_poprzedni";
+            this.btn_poprzedni.Size = new System.Drawing.Size(75, 23);
+            this.btn_poprzedni.TabIndex = 24;
+            this.btn_poprzedni.Text = "Previous";
+            this.btn_poprzedni.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(969, 534);
+            this.Controls.Add(this.btn_poprzedni);
+            this.Controls.Add(this.btn_nastepny);
+            this.Controls.Add(this.lKolejka);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Dane_pacjeta);
             this.Controls.Add(this.tp_Badania);
             this.Controls.Add(this.Badanie_label);
             this.Controls.Add(this.Imie_label);
@@ -197,6 +340,19 @@ namespace Badanie
         private System.Windows.Forms.Label Imie_label;
         private System.Windows.Forms.Label Badanie_label;
         private System.Windows.Forms.DateTimePicker tp_Badania;
+        private System.Windows.Forms.Label Dane_pacjeta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lKolejka;
+        private System.Windows.Forms.Button btn_nastepny;
+        private System.Windows.Forms.Button btn_poprzedni;
     }
 }
 
